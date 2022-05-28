2e40db424960cbe76eba0a6a84f4e1b278d2d0a0
Jae Shin <7jaeshin@gmail.com>
2022-05-27 20:33:16 -0400

    homepage initial setup

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,20 @@
+
+
+function HomePage() {
+
+    return (
+        <div>
+            <div className="homepage-slide">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.AFBZmAzd2ndaGrfmYrQunAHaE7%26pid%3DApi&f=1" alt="" ></img>
+            </div>
+            <div className="recent-activity">
+                <h1>Recent Activity</h1>
+            </div>
+            <div className="categories">
+                <h1>Categories</h1>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
