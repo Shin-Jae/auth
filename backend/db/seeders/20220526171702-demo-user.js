c716f9a9c9e4b47303d978268849b05f247c1a36
Jae Shin <7jaeshin@gmail.com>
2022-06-06 04:56:28 -0400

    seeders

--- backend/db/seeders/20220526171702-demo-user.js
+++ backend/db/seeders/20220526171702-demo-user.js
@@ -27,7 +27,19 @@ module.exports = {
         username: 'NextKingOfPirates',
         profileImg: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmugiwarashop.altervista.org%2Fwp-content%2Fuploads%2F2016%2F11%2Fmonkey_d__luffy___500_million__new_bounty_poster__by_anti_form30-d9b3k1w-600x922.jpg&f=1&nofb=1",
         hashedPassword: bcrypt.hashSync('password4')
-      }
+      },
+      {
+        email: 'thanos@thanos.io',
+        username: 'Thanos123',
+        profileImg: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.IJuTWnAhS2DK0mZm0mgd3gHaFj%26pid%3DApi&f=1",
+        hashedPassword: bcrypt.hashSync('password3')
+      },
+      {
+        email: 'bot@bot.com',
+        username: 'smiley',
+        profileImg: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.hc4O-KTD3NxOoPzkAmyQWAHaHa%26pid%3DApi&f=1",
+        hashedPassword: bcrypt.hashSync('password3')
+      },
     ], {});
   },
 
--- backend/db/seeders/20220527175936-review.js
+++ backend/db/seeders/20220527175936-review.js
@@ -10,44 +10,54 @@ module.exports = {
       */
     return queryInterface.bulkInsert('Reviews', [
       {
-        userId: 1,
+        userId: 2,
         groupId: 1,
         rating: 5,
-        review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
-        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
-        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Nkp0F_ZQVTN42T4Ns9lXmQHaEK%26pid%3DApi&f=1",
+        review: "Love the team, big fan, hopefully we can keep this core together and keep improving with our new coach",
+        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.GFqnfbrvTJdA3J6zAawIUgHaE8%26pid%3DApi&f=1",
+        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SMdPVdUvdro3_cBDefbn4QHaE8%26pid%3DApi&f=1",
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        userId: 4,
-        groupId: 2,
-        rating: 2.5,
-        review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
-        image1: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/5a6c7b50099273.5a64003d635c8.jpg",
-        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7%26pid%3DApi&f=1",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        userId: 1,
-        groupId: 4,
-        rating: 4,
-        review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
-        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7%26pid%3DApi&f=1",
-        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
+        userId: 3,
+        groupId: 1,
+        rating: 1,
+        review: "Poverty Club, win a trophy",
+        image1: null,
+        image2: null,
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
         userId: 2,
-        groupId: 3,
-        rating: 3,
-        review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
-        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7%26pid%3DApi&f=1",
-        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
+        groupId: 2,
+        rating: 4,
+        review: "The battle in New York destroyed my whole apartment complex, but I'm still glad the Avengers were there to save the day before it got much worse",
+        image1: null,
+        image2: null,
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
-        userId: 4,
+        userId: 5,
+        groupId: 2,
+        rating: 1,
+        review: "Hate you guys",
+        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.G7r36W2Sv4stPJNwbAm2cAHaHa%26pid%3DApi&f=1",
+        image2: null,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        userId: 5,
+        groupId: 2,
+        rating: 2,
+        review: "Miss me yet?",
+        image1: null,
+        image2: null,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 6,
         groupId: 5,
         rating: 4,
         review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
@@ -55,16 +65,37 @@ module.exports = {
         image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
         createdAt: new Date(),
         updatedAt: new Date()
-      }, {
-        userId: 3,
-        groupId: 1,
-        rating: 1,
+      },
+      {
+        userId: 6,
+        groupId: 6,
+        rating: 2,
         review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
         image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7%26pid%3DApi&f=1",
         image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
         createdAt: new Date(),
         updatedAt: new Date()
-      }
+      },
+      {
+        userId: 3,
+        groupId: 6,
+        rating: 3,
+        review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
+        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7%26pid%3DApi&f=1",
+        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 3,
+        groupId: 4,
+        rating: 4,
+        review: "Lorem ipsum dolor sit amet, et nec aperiri scribentur. Harum laudem scripta vim ei, vis latine pericula at. Ex duo iuvaret mandamus imperdiet. Mel exerci veritus splendide ne, vide homero vocibus qui no, an lorem tation perfecto nam. Ut eam oblique dissentiet.",
+        image1: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wytlw5AmN2HoCJ_kLGF1EgHaF7%26pid%3DApi&f=1",
+        image2: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.xJrCryY5WIzlb09HDiQFlAHaHR%26pid%3DApi&f=1",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
     ], {});
   },
 
